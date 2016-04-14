05104a1e0018535d185c7c3296b10f0694b92a58
Corey Jackson <cjacks04@syr.edu>
2016-04-14 15:15:15 -0400

    New code for Tf-idf

--- Hashtag/fileclean.py
+++ Hashtag/fileclean.py
@@ -37,5 +37,38 @@ test_data['name'].value_counts()
 # Projects and counts of number of hashtags
 raw_data['project_id'].value_counts()
 
+## TF-IDF functions
+import math
+from textblob import TextBlob as tb
+
+def tf(word, blob):
+    return blob.words.count(word) / len(blob.words)
+
+def n_containing(word, bloblist):
+    return sum(1 for blob in bloblist if word in blob)
+
+def idf(word, bloblist):
+    return math.log(len(bloblist) / (1 + n_containing(word, bloblist)))
+
+def tfidf(word, blob, bloblist):
+    return tf(word, blob) * idf(word, bloblist)
+
+##### Code to produce common terms
+
+# Put all words for a project in a single document (e.g., document3 = tb("word1 word2"))  
+
+
+# Create documents at T1 (3 month), T2 (6 month), T3 (9 month), T4 (12 month) 
+
+
+
+bloblist = [document1, document2, document3]
+for i, blob in enumerate(bloblist):
+    print("Top words in document {}".format(i + 1))
+    scores = {word: tfidf(word, blob, bloblist) for word in blob.words}
+    sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+    for word, score in sorted_words[:3]:
+        print("\tWord: {}, TF-IDF: {}".format(word, round(score, 5)))
+
 # Export data 
 raw_data.to_csv('Proejct_Tags.csv') #Change File name to project name. 
