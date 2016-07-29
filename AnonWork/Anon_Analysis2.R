2833cfbfb8a52c2f9d240b75d19f073211756abe
Corey Jackson <cjacks04@syr.edu>
2016-07-29 14:15:31 -0700

    New File for anonymous work

---
+++ AnonWork/Anon_Analysis2.R
@@ -0,0 +1,15 @@
+# Code for analyzing Zooniverse anonymous work data 
+# Corey Jackson, School of Information Studies, Syracuse University, July 2016
+
+
+# Install packages 
+install.packages("ggplot2")
+install.packages("plyr")
+install.packages("reshape2")
+install.packages("lubridate")
+
+# Load libraries
+library(ggplot2)
+library(plyr)
+library(reshape2)
+library(lubridate)
