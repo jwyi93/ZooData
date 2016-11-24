70b3696ded86680feeeba13fd3626e322101eb80
Corey Jackson <cjacks04@syr.edu>
2016-11-24 12:20:41 -0500

    Finalized Code and added visualizations

--- GravitySpy/Promotion/promotion.R
+++ GravitySpy/Promotion/promotion.R
@@ -5,14 +5,95 @@
 ## Promotion Analysis for Gravity Spy
 # After users are promoted do they still do analysis in other workflows
 
+classifications <- read.csv("~/Dropbox/INSPIRE/Data/Promotion Analysis/classifications.csv")
 
 library(plyr)
+library(reshape2)
+
+classifications$year <- substring(classifications$start, 2, 11)
+classifications$time <- substring(classifications$start, 13, 20)
+classifications$Date <- paste(classifications$year,classifications$time) # Automatically converts to date
+classifications$Date <- as.POSIXct(as.character(classifications$Date),format="%Y-%m-%d %H:%M:%S") 
+classifications$year <- NULL
+classifications$time <- NULL
 
 # Generate file with user workflow name and promotion date
-promotions <- ddply(classifications, c("userID","workflow"),
-	Date = min(datetime))
+promotions <- ddply(classifications, c("userID","workflow"),summarise,
+	Date = min(Date),
+	Classification = min(Classifications),
+	Session = min(Session))
+
+## Build promotion tables and merge promotino tables with classification table
+
+#1610: Neutron Star Mountain
+#1934: Galactic Supernova
+#1935: Neutron Star Merger
+#2360: Black Hole Merger
+#2117: Universe Cosmic Background
+
+promotions1610 <- promotions[ which(promotions$workflow==1610), ]
+promotions1610 <- rename(promotions1610, replace = c("Date" = "promotion1610"))
+classifications <- merge(classifications,promotions1610[, c("userID","promotion1610")], 
+	by=("userID"), all.x=TRUE)
+
+promotions1934 <- promotions[ which(promotions$workflow==1934), ]
+promotions1934 <- rename(promotions1934, replace = c("Date" = "promotion1934"))
+classifications <- merge(classifications,promotions1934[, c("userID","promotion1934")], 
+	by=("userID"), all.x=TRUE)
+
+promotions1935 <- promotions[ which(promotions$workflow==1935), ]
+promotions1935 <- rename(promotions1935, replace = c("Date" = "promotion1935"))
+classifications <- merge(classifications,promotions1935[, c("userID","promotion1935")], 
+	by=("userID"), all.x=TRUE)
+
+promotions2360 <- promotions[ which(promotions$workflow==2360), ]
+promotions2360 <- rename(promotions2360, replace = c("Date" = "promotion2360"))
+classifications <- merge(classifications,promotions2360[, c("userID","promotion2360")], 
+	by=("userID"), all.x=TRUE)
+
+promotions2117 <- promotions[ which(promotions$workflow==2117), ]
+promotions2117 <- rename(promotions2117, replace = c("Date" = "promotion2117"))
+classifications <- merge(classifications,promotions2117[, c("userID","promotion2117")], 
+	by=("userID"), all.x=TRUE)
+
+#### Code wouldn't work because of dates so exported to Excel to get level user was in when annotation submitted  !!!!!!!
+write.csv(classifications, "classifications_promotion.csv")
+#=IF(AF2>=AK2,"Universe Cosmic Background",IF(AF2>=AJ2,"Black Hole Merger",IF(AF2>=AI2,"Neutron Star Merger",IF(AF2>=AH2,"Galactic Supernova",IF(AF2>=AG2,"Neutron Star Mountain",1)))))
+
+## Not working. Date Error
+#classifications$UserLevel <- ifelse(as.POSIXct(classifications$datetime) >= as.POSIXct(classifications$promotion2117),"Universe Cosmic Background",
+#       ifelse(as.POSIXct(classifications$datetime) >= as.POSIXct(classifications$promotion2360), "Black Hole Merger",
+#            ifelse(as.POSIXct(classifications$datetime) >= as.POSIXct(classifications$promotions1935), "Neutron Star Merger",
+#                ifelse(as.POSIXct(classifications$datetime) >= as.POSIXct(classifications$promotions1934), "Galactic Supernova","Neutron Star Mountain"))))
+
+# Re-import file with userlevel
+classifications_promotion <- read.csv("~/Desktop/MessageExperiment/classifications_promotion.csv")
+classifications_promotion$workflow <- as.factor(classifications_promotion$workflow)
+
+# Recode new Variables
+classifications_promotion$SubmitLevel[classifications_promotion$workflow == "1610"] <- "Neutron Star Mountain"
+classifications_promotion$SubmitLevel[classifications_promotion$workflow == "1934"] <- "Galactic Supernova"
+classifications_promotion$SubmitLevel[classifications_promotion$workflow == "1935"] <- "Neutron Star Merger"
+classifications_promotion$SubmitLevel[classifications_promotion$workflow == "2360"] <- "Black Hole Merger"
+classifications_promotion$SubmitLevel[classifications_promotion$workflow == "2117"] <- "Universe Cosmic Background"
+
+pdf("PromotionSubmit.pdf", height=7, width=9)
+hist_prom <- ggplot(classifications_promotion, aes(x=SubmitLevel, fill=UserLevel))
+hist_prom + 
+geom_bar() + 
+theme (
+	axis.text.x = element_text(size=10, angle=45)
+	)
+dev.off()
 
 
+promotions_stats <- ddply(classifications_promotion, c("UserLevel"),summarise,
+	Users = length(unique(login)),
+	Neutron_Star_Mountain = sum(SubmitLevel=="Neutron Star Mountain"),
+	Galactic_Supernova = sum(SubmitLevel=="Galactic Supernova"),
+	Neutron_Star_Merger = sum(SubmitLevel=="Neutron Star Merger"),
+	Black_Hole_Merger = sum(SubmitLevel=="Black Hole Merger"),
+	Universe_Cosmic_Background = sum(SubmitLevel=="Universe Cosmic Background"))
 
 
 
