ad85bbb7762b6e989330e1be722705146c7bb72a
Corey Jackson <cjacks04@syr.edu>
2016-12-07 15:08:18 -0500

    Adding function

--- GravitySpy/Message-Experiment/rmdupes.py
+++ GravitySpy/Message-Experiment/rmdupes.py
@@ -1,16 +1,19 @@
 # Mabi Harandi
 # Syracuse University, 2016
 
-#### Remove duplicates from file
-def no_duplicate(filename):
-   df1 = pd.read_csv(filename)
-   df2 = df1.drop_duplicates(subset= ['userID'])
-   temp = filename.title()
-   m = re.search("(.+?).Csv", temp)
-   if m:
-       file_name = m.group(1)
-   with open('%s-no-duplicate.csv' %file_name, 'w') as output:
-       df2.to_csv(output, sep=',')
+import pandas as pd
+import re
 
-filename = "classifications_with_timestamp.csv"
+def no_duplicate(filename):
+    df1 = pd.read_csv(filename)
+    df2 = df1.drop_duplicates(subset= ['userID', 'timeToClass'])
+    temp = filename.title()
+    m = re.search("(.+?).Csv", temp)
+    if m:
+        file_name = m.group(1)
+    with open('%s-no-duplicate.csv' %file_name, 'w') as output:
+        df2.to_csv(output, sep=',')
+
+#filename = "classifications_with_timestamp.csv"
+filename = "gs_class_message_20161206.csv"
 no_duplicate(filename)
