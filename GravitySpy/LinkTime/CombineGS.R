a5a11a05f65d750248ff4bd5230557fefd7d8a76
Corey Jackson <cjacks04@syr.edu>
2016-12-13 11:27:33 -0500

    Update CombineGS.R

--- GravitySpy/LinkTime/CombineGS.R
+++ GravitySpy/LinkTime/CombineGS.R
@@ -1,1 +1,9 @@
+# Corey Jackson
+# Syracuse University, 2016
 
+# Merging Scotty's classification file with Zooniverse dataset. Timestamps are missing from Scotty's data. 
+
+gravity.spy.classifications_1 <- read.csv("~/Desktop/Research Projects/MessagingExperiment/gravity-spy-classifications_1.csv")
+classifications_metadata <- read.csv("~/Downloads/classifications_metadata.csv")
+
+gs_classifications12216 <- merge(gravity.spy.classifications_1,classifications_metadata, by.x=("classificiation_id"), by.y=("classificationID"), all.x=FALSE)
