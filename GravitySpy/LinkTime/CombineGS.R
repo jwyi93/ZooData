82cccb9cb7c1292602b04b3b0ce78312b0547f2a
Corey Jackson <cjacks04@syr.edu>
2016-12-13 12:15:26 -0500

    Update CombineGS.R

--- GravitySpy/LinkTime/CombineGS.R
+++ GravitySpy/LinkTime/CombineGS.R
@@ -6,4 +6,4 @@
 gravity.spy.classifications_1 <- read.csv("~/Desktop/Research Projects/MessagingExperiment/gravity-spy-classifications_1.csv")
 classifications_metadata <- read.csv("~/Downloads/classifications_metadata.csv")
 
-gs_classifications12216 <- merge(gravity.spy.classifications_1,classifications_metadata, by.x=("classificiation_id"), by.y=("classificationID"), all.x=FALSE)
+gs_classifications12216 <- merge(gravity.spy.classifications,classifications_metadata, by.x=("classificiation_id"), by.y=("classificationID"), all.x=FALSE)
